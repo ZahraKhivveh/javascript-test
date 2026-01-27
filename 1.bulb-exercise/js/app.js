2a7f1d40ddc5f4312d0c5a1443b9fce1cd9b0c8a
zahra khivveh <zahrakhivveh@gmail.com>
2026-01-27 22:57:05 +0330

    1404/11/07

--- 1.bulb-exercise/js/app.js
+++ 1.bulb-exercise/js/app.js
@@ -1,16 +1,10 @@
-const img = document.querySelector("img");
-const btn = document.querySelector(".button");
+let btn=document.querySelector(".button");
+let img=document.querySelector(".oo-omg");
 
-let isOn = false; // Flag
+let isON=false;
 
-btn.addEventListener("click", function () {
-  if (isOn) {
-    img.setAttribute("src", "./pics/bulboff.gif");
-    btn.innerHTML = "لامپ را روشن کنید";
-    isOn = false;
-  } else {
-    img.setAttribute("src", "./pics/bulbon.gif");
-    btn.innerHTML = "لامپ را خاموش کنید";
-    isOn = true;
-  }
-});
+btn.addEventListener("click" , function(){
+  btn.innerHTML="Turn off the bulb"
+
+}
+);
