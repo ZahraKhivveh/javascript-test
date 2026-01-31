c4a727d6d8ce899e97e3a06d1f98bcb178d1849c
zahra khivveh <zahrakhivveh@gmail.com>
2026-01-31 16:16:36 +0330

    1404/11/11

--- 5.onChange event-exercise/js/app.js
+++ 5.onChange event-exercise/js/app.js
@@ -1,35 +1,35 @@
 let provinceInput = document.querySelector(".province-select");
 let cityInput = document.querySelector(".city-select");
 
-let all ={
-  fars:["شیراز" , "زرقان" , "لامرد" , "نوراباد"],
-  khoozestan:["اهواز" , "ابادان" , "حمیدیه"],
-khorasan:["مشهد" , "بیرجند" , "بجنورد"],
-isfahan:["اصفهان" , "نجف اباد" , "کاشان"],
-mazandaran:["ساری" , "امل" ,"بابل"],
+let cities ={
+  فارس:["شیراز" , "زرقان" , "لامرد" , "نوراباد"],
+  خوزستان:["اهواز" , "ابادان" , "حمیدیه"],
+خراسان:["مشهد" , "بیرجند" , "بجنورد"],
+اصفهان:["اصفهان" , "نجف اباد" , "کاشان"],
+مازندران:["ساری" , "امل" ,"بابل"],
 };
+let provinces = Object.keys(cities);  /array/
+
+let newOption;
+provinces.forEach(function(province) {
+  newOption = document.createElement("option");
+  newOption.className= "px-4";
+  newOption.value = province;
+  newOption.innerHTML=province;
+  provinceInput.append(newOption);
+});
 
 provinceInput.addEventListener("change" , function(){
   let province = provinceInput.value ;
+  let cityOf = cities[province];
+ 
+  cityInput.innerHTML="";
+  let newCityOption;
+cityOf.forEach(function(city) {
+  newCityOption = document.createElement("option");
+  newCityOption.className= "px-4";
+  newCityOption.value = city;
+  newCityOption.innerHTML=city;
+  cityInput.append(newCityOption);
 });
-//* onChange (input - selectBox)
-
-const textInput = document.querySelector(".text-input");
-const checkInput = document.querySelector(".check-input");
-const citiesSelectBox = document.querySelector("#cities");
-
-// textInput.addEventListener("change", function () {
-//   console.log("Input Changed");
-// });
-
-// checkInput.addEventListener("change", function () {
-//   console.log("CheckBox Changed ->", checkInput.checked);
-// });
-
-// citiesSelectBox.addEventListener("change", function () {
-//   console.log("City Changed -> ", citiesSelectBox.value);
-// });
-
-function selectCity() {
-  console.log("City Changed -> ", citiesSelectBox.value);
-}
+});
